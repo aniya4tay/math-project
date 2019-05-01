8841e986ba6f1b4c9896105bff49d6a089ee9d2b
aniya4tay <47803667+aniya4tay@users.noreply.github.com>
2019-05-01 13:22:32 -0400

    Update equation.c

--- equation.c
+++ equation.c
@@ -8,20 +8,20 @@ int main(void){
     int v=0;
     char unknown [8];
     
-    printf("What are you solving for Voltage equation?(P,I,or V)?\n");
+    printf("What are you solving for Voltage equation?(V,I,or R)?\n");
     scanf("%c", unknown);
     
-    if( *unknown=='v'){
+    if( *unknown=='V'){
         i=get_int ("I is:\n");
         r=get_int ("R is:\n");
         printf ("V is:%f \n",(float) i*r);
         }
-    if (*unknown=='i'){
+    if (*unknown=='I'){
         v=get_int("V is:\n");
         r=get_int("R is:\n");
         printf ("I is:%f\n",(float) v/r);
         }
-    if (*unknown== 'r'){
+    if (*unknown== 'R'){
         v=get_int("V is:\n");
         i=get_int("I is:\n");
         printf ("R is:%f\n",(float) 
